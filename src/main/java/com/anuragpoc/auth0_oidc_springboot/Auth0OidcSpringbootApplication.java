77e583317e56ceea3260414a10d645125374ad47
captainhawkeye <anusriofficial@gmail.com>
2024-07-06 18:38:46 +0530

    Initial Commit

---
+++ src/main/java/com/anuragpoc/auth0_oidc_springboot/Auth0OidcSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.anuragpoc.auth0_oidc_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0OidcSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0OidcSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuragpoc/auth0_oidc_springboot/Auth0OidcSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuragpoc.auth0_oidc_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0OidcSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
